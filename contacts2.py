afebf99fd208fa37038417f68749300211f41510
Szymek <szymon.rzadca@gmail.com>
2018-06-19 10:29:50 +0200

    committing changes to a database and accessing database contents from another script

--- contacts2.py
+++ contacts2.py
@@ -2,7 +2,10 @@ import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
 
-for row in db.execute("SELECT * FROM contacts"):
-    print(row)
+for name, phone, email in db.execute("SELECT * FROM contacts"):
+    print(name)
+    print(phone)
+    print(email)
+    print("-" * 20)
 
 db.close()
