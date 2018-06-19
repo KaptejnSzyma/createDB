59a0e9d11e570a9e4c268dec6cc0382fad1b28c7
Szymek <szymon.rzadca@gmail.com>
2018-06-19 10:34:14 +0200

    updating record with cursors

--- contacts2.py
+++ contacts2.py
@@ -2,6 +2,13 @@ import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
 
+update_sql = "UPDATE contacts SET email = 'update@update.com' WHERE contacts.phone = 1234"
+update_cursor = db.cursor()
+update_cursor.execute(update_sql)
+print("{} rows updated".format(update_cursor.rowcount))
+
+update_cursor.close()
+
 for name, phone, email in db.execute("SELECT * FROM contacts"):
     print(name)
     print(phone)
